b0231d479011698fe2395553e3735211310c5264
Mohammed Sinan Khan <msinankhan@gmail.com>
2026-01-31 19:00:03 +0300

    Finished the checkpoint manager script

--- nanollm/checkpoint_manager.py
+++ nanollm/checkpoint_manager.py
@@ -34,5 +34,134 @@ def _patch_missing_keys(model_data,model_config):
         model_data["x0_lambda"] = torch.zeros(n_layer)
         log0(f"Patching missing x0_lambda in model data to 0.0 .")
 
+def save_checkpoint(checkpoint_dir,step,model_data, optimizer_data, meta_data,rank=0):
 
+    if rank==0:
+        os.makedirs(checkpoint_dir, exist_ok=True)
+        model_path=os.path.join(checkpoint_dir, f"model_{step:06d}.pt")
+        torch.save(model_data,model_path)
+        logger.info(f"Saved model parameters to: {model_path}")
+
+        meta_path=os.path.join(checkpoint_dir, f"model_{step:06d}.json")
+        with open(meta_path,'w', encoding="utf-8") as f:
+            json.dump(meta_data,f, indent=2)
+
+        logger.info(f"Saved meta data to: {meta_path}.")
+
+    if optimizer_data is not None:
+        os.makedirs(checkpoint_dir, exist_ok=True)
+        optimizer_path=os.path.join(checkpoint_dir, f"optim_{step:06d}_rank{rank:d}.pt")
+        torch.save(optimizer_data, optimizer_path)
+        logger.info(f"Saved optimizer state to {optimizer_path}.")
+
+
+def load_checkpoints(checkpoint_dir,step,device, load_optimizer=False, rank=0):
+    model_path=os.path.join(checkpoint_dir, f"model_{step:06d}.pt")
+    model_data=torch.load(model_path, map_location=device)
+
+    optimizer_data=None
+    if load_optimizer:
+        optimizer_path=os.path.join(checkpoint_dir, f"optim_{step:06d}_rank{rank:d}.pt")
+        optimizer_data=torch.load(optimizer_path, map_location=device)
+
+    meta_path=os.path.join(checkpoint_dir, f"model_{step:06d}.json")
+    with open(meta_path, 'r',encoding="utf-8") as f:
+        meta_data=json.load(f)
+    return model_data,optimizer_data,meta_data
+
+
+
+def build_model(checkpoint_dir,step, device,phase):
+    assert phase in ["train","eval"], f"The phase must be either train or eval:{phase}."
+
+    model_data,optimizer_data,meta_data=load_checkpoints(checkpoint_dir,step,device, load_optimizer=False)
+
+    if device.type in {"cpu"}:
+        model_data={k:v.float() if v.dtype==torch.bfloat16 else v 
+                     for k,v in model_data.items()}
         
+
+    model_data={k.removeprefix("_orig_mod."): v for k,v in model_data.items()}
+    model_config_kwargs=meta_data["model_config"]
+    _patch_missing_config_keys(model_config_kwargs)
+    log0(f"Building model with config: {model_config_kwargs}")
+    model_config=GPTConfig(**model_config_kwargs)
+    _patch_missing_keys(model_data, model_config)
+
+    with torch.device("meta"):
+        model=GPT(model_config)
+
+
+    model.to_empty(device=device)
+    model.init_weights() #Some model components (e.g. rotary embeddings) are buffers and are not part of state_dict. They need initialization logic to run.
+                        #Hence we have to run init_weights() in spite of most parameters getting overwritten.
+    model.load_state_dict(model_data,strict=True,assign=True)
+
+    if phase=="eval":
+        model.eval()         #disables dropout, freezes layernorm behavior
+    else:
+        model.train()        #enables stochastic layers.
+
+    tokenizer=get_tokenizer()
+
+    assert tokenizer.get_vocab_size() ==model_config_kwargs["vocab_size"], f"Tokenizer Vocab Size {tokenizer.get_vocab_size()} doesn't match config vocab size {model_config_kwargs["vocab_size"]}"
+    return model,tokenizer,meta_data
+        
+def find_largest_model(checkpoint_dir):
+    model_tags=[f for f in os.listdir(checkpoint_dir) if  os.path.isdir(os.path.join(checkpoint_dir,f))]
+    if not model_tags:
+        raise FileNotFoundError(f"No checkpoints found in {checkpoint_dir}")
+    
+    candidates=[]
+    for model_tag in model_tags:
+        match=re.match(r"d(\d+)",model_tag)
+
+        if match:
+            model_depth=int(match.group(1))
+            candidates.append(model_depth,model_tag)
+
+    if candidates:
+        candidates.sort(key=lambda x:x[0], reverse=True)
+        return candidates[0][1]
+    
+    model_tags.sort(key=lambda x:os.path.getmtime(os.path.join(checkpoint_dir,x)), reverse=True)
+    return model_tags[0]
+
+def find_last_step(checkpoint_dir):
+    checkpoint_files=glob.glob(os.path.join(checkpoint_dir,"model_*.pt"))
+    if not checkpoint_files:
+        raise FileNotFoundError(f"No checkpoints found in {checkpoint_files}")
+    last_step=int(max(os.path.basename(f).split("_")[-1].split(".")[0] for f in checkpoint_files))
+    return last_step
+
+def load_model_from_dir(checkpoint_dir, device, phase, model_tag=None, step=None):
+    if model_tag is None:
+        model_tag=find_largest_model(checkpoint_dir)
+        log0(f"No model tag provided, guessing the model tag:{model_tag}")
+
+    checkpoint_dir=os.path.join(checkpoint_dir,model_tag)
+
+    if step is None:
+        step=find_last_step(checkpoint_dir)
+
+    assert step is not None, f"No checkpoints found in {checkpoint_dir}"
+
+    log0(f"Loading model from {checkpoint_dir} with step {step}")
+
+    model,tokenizer, meta_data=build_model(checkpoint_dir,step,device,phase)
+    return model,tokenizer,meta_data
+
+
+def load_model(source,*args,**kwargs):
+    model_dir={
+        "base":"base_checkpoint",
+        "mid":"mid_checkpoint",
+        "sft":"chatsft_checkpoint",
+        "rl": "chatrl_checkpoint"
+    }[source]
+
+    base_dir=get_base_dir()
+    checkpoint_dir=os.path.join(base_dir,model_dir)
+    return load_model_from_dir(checkpoint_dir,*args,**kwargs)
+
+
