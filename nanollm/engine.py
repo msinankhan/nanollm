d2bdd5d75816ec6af7262509dd1c22754236d6b1
Mohammed Sinan Khan <msinankhan@gmail.com>
2026-02-02 12:51:32 +0300

    KV Cache and next token gen

--- nanollm/engine.py
+++ nanollm/engine.py
@@ -65,3 +65,69 @@ def use_calculator(exp):
     
     return eval_with_timeout(exp)
 
+
+class KVCache:
+
+    def __init__(self,batch_size,num_heads,seq_len, head_dim,num_layers,device, dtype):
+        self.batch_size=batch_size
+        self.max_seq_len=seq_len
+        self.n_layers=num_layers
+        self.n_heads=num_heads
+        self.head_dim=head_dim
+
+        self.k_cache=torch.zeros(num_layers,batch_size,seq_len,num_heads,head_dim,device=device,dtype=dtype)
+        self.v_cache=torch.zeros(num_layers,batch_size,seq_len,num_heads,head_dim,device=device,dtype=dtype)
+
+
+        self.cache_seqlens=torch.zeros(batch_size,device=device,dtype=torch.int32) #Current seq lenght. 
+                                                                                  # Helps model know which part of the cache is valid.
+                                                                                  #K/V values can be zero legitimately , hence this helps in identifying 
+
+    def reset(self):
+        self.cache_seqlens.zero_() #Resets the cache to 0.
+
+    def get_pos(self):
+        return self.cache_seqlens[0].item() #item() returns the tensor as a python number
+    
+    def get_layer_cache(self,layer_idx):
+        return self.k_cache[layer_idx], self.v_cache[layer_idx]
+    
+    def advance(self,num_tokens):
+        self.cache_seqlens+=num_tokens #Advances the cache by num_tokens
+
+
+    def prefill(self,other):
+
+        """Copies KV from another cache into this one.
+        Will be used when we want to generate multiple samples in parallel. """
+        assert self.get_pos==0, f"Cannot fill a non-empty cache"
+        assert self.n_layers==other.n_layers and self.n_heads==other.n_heads and self.head_dim==self.head_dim
+        assert self.max_seq_len>=other.max_seq_len
+
+        other_pos=other.get_pos()
+
+        self.k_cache[:,:,:other_pos,:,:]= other.k_cache[:,:,:other_pos,:,:]
+        self.v_cache[:,:,:other_pos,:,:] = other.v_cache[:,:,:other_pos,:,:]
+        self.cache_seqlens.fill_(other_pos)
+
+
+@torch.inference_mode()
+def sample_next_token(logits,rng,temperature=1.0,top_k=None):
+    assert temperature>=0.0, f"Temperature has to be non-negative:{temperature}"
+
+    if temperature==0:
+        return torch.argmax(logits,dims=-1,keepdim=True)
+    if top_k is not None and top_k>0:
+        k=min(top_k,logits.size(-1))
+        vals,idx=torch.topk(logits,k,dim=-1)
+        vals=vals/temperature #Temperature rescales relative differences, higher_temp-> flatter distribution; lower_temp->peakier distribution.
+        probs=F.softmax(vals,dim=-1)
+        choice=torch.multinomial(probs,num_samples=1, generator=rng)
+        return idx.gather(1,choice) #choice is the index in the top_k list, but we need the original token_id.
+    else:
+        logits=logits/temperature
+        probs=F.softmax(logits,dim=-1)
+        return torch.multinomial(probs,num_samples=1,generator=rng)
+    
+
+
