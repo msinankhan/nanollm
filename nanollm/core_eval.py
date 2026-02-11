325dc995764f2bb8aff6c3d4ea5d3bc8eb27970c
Mohammed Sinan Khan <msinankhan@gmail.com>
2026-02-11 13:08:55 +0300

    Forward pass and single sample evaluation.

--- nanollm/core_eval.py
+++ nanollm/core_eval.py
@@ -53,12 +53,33 @@ def render_prompts_schema(item,continuation_delimiter,fewshot_examples=None):
     
 
 def render_prompt_lm(item,continuation_delimiter,fewshot_examples=None):
-    template_str="""
-{%- for example in fewshot_examples -%}
-{{ example.context | trim }}{{ continuation_delimiter }}{{ example.continuation }}
+    """ There are no choices
 
-{% endfor -%}
-{{ item.context | trim }}{{ continuation_delimiter }}{% if include_continuation %}{{ item.continuation }}{% endif %}""".strip()
+        There is one continuation
+
+        The question is:
+        👉 Given a prefix, does the model correctly predict the continuation?
+
+
+        For LM evaluation, we need two versions of the prompt:
+
+        Prompt WITHOUT continuation
+        → Used to identify where the continuation begins in token space
+
+        Prompt WITH continuation
+        → Used to compute loss / predictions on the continuation tokens
+
+        That’s why this function returns two prompts.
+
+    """
+
+
+    template_str="""
+    {%- for example in fewshot_examples -%}
+    {{ example.context | trim }}{{ continuation_delimiter }}{{ example.continuation }}
+
+    {% endfor -%}
+    {{ item.context | trim }}{{ continuation_delimiter }}{% if include_continuation %}{{ item.continuation }}{% endif %}""".strip()
     template=Template(template_str)
     fewshot_examples=fewshot_examples or []
     context={
@@ -75,7 +96,20 @@ def render_prompt_lm(item,continuation_delimiter,fewshot_examples=None):
 
 
 def find_common_length(token_sequence,direction='left'):
-    """Length of the common prefix or suffix across token sequences."""
+    """ Length of the common prefix or suffix across token sequences.
+        Given multiple token sequences, we want to know:
+
+            How many tokens are identical at the start (prefix), or
+
+            How many tokens are identical at the end (suffix)
+
+        This is essential because:
+
+            Multiple choice → same prefix, different endings
+
+            Schema → different prefixes, same suffix
+
+    """
     
     min_len=min(len(seq) for seq in token_sequence)
     indices={
@@ -91,9 +125,139 @@ def find_common_length(token_sequence,direction='left'):
     return min_len
 
 def stack_sequences(tokens,pad_token_id):
+    """Stack up a list of token sequences, pad to longest on the right"""
     bsz,seq_len=len(tokens),max(len(x) for x in tokens)
     input_ids=torch.full((bsz,seq_len),pad_token_id,dtype=torch.long)
     for i,x in enumerate(tokens):
         input_ids[i,:len(x)] =torch.tensor(x,dtype=torch.long)
 
     return input_ids
+
+
+def batch_sequences_mc(tokenizer,prompts):
+    tokens=tokenizer(prompts,prepend=tokenizer.get_bos_token())
+    answer_start_idx=find_common_length(tokens,direction='left')
+    start_idx=[answer_start_idx] * len(prompts)    # Because the prompts are the same, but we have different answers
+    end_idx=[len(x) for x in tokens]
+    
+    return tokens, start_idx, end_idx
+
+def batch_sequences_schema(tokenizer,prompts):
+
+    """In Schema, the context varies but the continuation remains the same.
+       In schema tasks, we measure how well each context predicts that continuation"""
+
+    tokens=tokenizer(prompts, prepend=tokenizer.get_bos_token())
+
+    suffix_length=find_common_length(tokens,direction='right')
+    end_idx=[len(x) for x in tokens]
+    start_idx=[ei - suffix_length for ei in end_idx]
+    return tokens, start_idx, end_idx
+
+
+
+def batch_sequences_lm(tokenizer,prompts):
+    tokens=tokenizer(prompts, prepend=tokenizer.get_bos_token())
+    tokens_without,tokens_with=tokens
+    start_idx,end_idx=len(tokens_without),len(tokens_with)
+    assert start_idx<end_idx , "Prompt_without is supposed to be a prefix of prompt with."
+    assert tokens_without==tokens_with[:start_idx], "Prompt_without is supposed to be a prefix of prompt_with"
+
+    return [tokens_with], [start_idx], [end_idx]
+
+
+
+@torch.no_grad()
+def forward(model,input_ids):
+    batch_size,seq_len=input_ids.size()
+    outputs=model(input_ids)
+
+    target_ids=torch.roll(input_ids,shifts=-1, dims=1)
+
+    losses=torch.nn.functional.cross_entropy(
+        outputs.view(batch_size*seq_len,-1),
+        target_ids.view(batch_size*seq_len),
+        reduction='none'
+    ).view(batch_size,seq_len)
+
+    losses[:,-1]=float('nan')
+    predictions=outputs.argmax(dim=-1)
+    return losses,predictions
+
+
+@torch.no_grad()
+def evaluate_example(idx,model,tokenizer,data,device,task_meta):
+    item=data[idx]
+    task_type=task_meta["task_type"]
+    num_fewshot=task_meta["num_fewshot"]
+    continuation_delimiter=task_meta["continuation_delimiter"]
+
+    fewshot_examples=[]
+
+    if num_fewshot>0:
+        rng=random.Random(1234+idx)
+        available_indices=[i for i in range(len(data)) if i!=idx]
+        fewshot_indices=rng.sample(available_indices,num_fewshot)
+        fewshot_examples=[data[i] for i in fewshot_indices]
+
+
+    if task_type=="multiple_choice":
+        prompts=render_prompt_mc(item,continuation_delimiter,fewshot_examples)
+        tokens,start_idx,end_idx=batch_sequences_mc(tokenizer,prompts)
+    elif task_type=="schema":
+        prompts=render_prompts_schema(item,continuation_delimiter,fewshot_examples)
+        tokens,start_idx,end_idx=batch_sequences_schema(tokenizer,prompts)
+    elif task_meta=="language_modeling":
+        prompts=render_prompt_lm(item,continuation_delimiter,fewshot_examples)
+        tokens,start_idx,end_idx=batch_sequences_lm(tokenizer,prompts)
+
+    else:
+        raise ValueError(f"Unsupported task type:{task_type}")
+    
+    if hasattr(model,'max_seq_len') and model.max_seq_len is not None:
+        max_tokens=model.max_seq_len
+        new_tokens,new_start_idx,new_end_idx=[],[],[]
+        for t,s,e in enumerate(zip(start_idx,end_idx)):
+            if len(t)>max_tokens:
+                num_to_crop=len(t)-max_tokens
+                new_tokens.append(t[-max_tokens:])
+                new_start_idx.append(s-num_to_crop)
+                new_end_idx.append(e-num_to_crop)
+                assert s- num_to_crop>=0, f"This shouldn't happen, righttt?"
+                assert e - num_to_crop>=0, f"This should happen either, right?"
+
+            else:
+                new_tokens.append(t)
+                new_start_idx.append(s)
+                new_end_idx.append(e)
+
+        tokens,start_idx,end_idx=new_tokens,new_start_idx,new_end_idx
+
+
+
+    pad_token_id=tokenizer.get_bos_token()
+    input_ids=stack_sequences(tokens,pad_token_id)
+    input_ids=input_ids.to(device)
+
+
+    losses,predictions=forward(model,input_ids)
+
+
+    if task_type == 'language_modeling':
+        si=start_idx[0]
+        ei=end_idx[0]
+
+        predicted_tokens=predictions[:,si-1:ei-1]
+        actual_token=input_ids[0,si:ei]
+        is_correct=torch.all(predicted_tokens==actual_token).item()
+
+
+    elif task_type in ['multiple_choice','schema']:
+        mean_losses=[losses[i,si-1:ei-1].mean().item()
+                     for i ,(si,ei) in enumerate(zip(start_idx,end_idx))]
+        
+        pred_idx=mean_losses.index(min(mean_losses))
+        is_correct = pred_idx==item['gold']
+    else:
+        raise ValueError(f"Unsupported task type: {task_type}")
+    return is_correct
