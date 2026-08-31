8473829988fb7ec907325065a8dc9833c9217488
msinankhan <msinankhan@gmail.com>
2026-08-31 12:58:13 +0300

    Finished Chat RL script!

--- scripts/chat_rl.py
+++ scripts/chat_rl.py
@@ -94,7 +94,7 @@ def get_batch():
 
         # num_sampling_steps = args.num_samples //args.device_batch_size
 
-        for sample_start in range(0, args.num_samples, args.device_batch_size, ):
+        for sample_start in range(0, args.num_samples, args.device_batch_size):
             current_batch_size = min( args.device_batch_size, args.num_samples - sample_start,)
             sampling_step = sample_start // args.device_batch_size
 
@@ -149,7 +149,179 @@ def get_batch():
         yield generated_token_sequences, inputs, targets, rewards, advantages
 
 
-        
+
+def run_gsm8k_eval(task,tokenizer,engine,
+                   max_examples=None, num_samples=1,
+                   max_completion_tokens=256, temperature =0.0,
+                   top_k=50):
+
+    max_examples = min(max_examples, len(task)) if max_examples is not None else len(task)
+    for idx in range(ddp_rank,max_examples, ddp_world_size):
+        conversation = task[idx]
+        tokens = tokenizer.render_for_completion(conversation)
+        prefix_length = len(tokens)
+
+        assert num_samples <= args.device_batch_size
+
+        generated_token_sequences, masks = engine.generate_batch(
+            tokens,num_samples=num_samples,
+            max_tokens = max_completion_tokens,
+            temperature= temperature,
+            top_k=top_k
+        )
+
+        outcomes=[]
+
+        for sample_tokens in generated_token_sequences:
+            generated_tokens = sample_tokens[prefix_length:]
+            generated_text = tokenizer.decode(generated_tokens)
+            is_correct = task.evaluate(conversation, generated_text)
+            outcomes.append({
+                "is_correct" : is_correct
+            })
+        record ={
+                "idx" : idx,
+                "outcomes" : outcomes,
+            }
+
+        yield record
+
+
+optimizer = model.setup_optimizer(
+    unembedding_lr=args.unembedding_lr,
+    embedding_lr=args.embedding_lr,
+    matrix_lr=args.matrix_lr,
+    weight_decay=args.weight_decay,
+)
+
+for group in optimizer.param_groups:
+    group["lr"] = group["lr"] * args.init_lr_frac
+    group["initial_lr"] = group['lr']
+
+def get_lr_multiplier(it):
+    lrm = 1.0 - it / num_steps
+    return lrm
+
+print0(f"Total Sequences per step: {args.examples_per_step * args.num_samples}")
+assert args.examples_per_step % ddp_world_size ==0, "Desired examples per step must be divisible by the number of ranks."
+examples_per_rank = args.examples_per_step // ddp_world_size
+print0(f"Calculated examples per rank: {examples_per_rank}")
+
+batch_iterator = get_batch()
+
+for step in range(num_steps):
+
+    if step % args.eval_every ==0:
+        model.eval()
+        passk = torch.zeros(args.device_batch_size, device=device)
+        records_iter = run_gsm8k_eval(val_task, tokenizer, engine, num_samples=args.device_batch_size, max_examples=args.eval_examples, temperature=1.0)
+        records = list(records_iter)
+
+        for k in range(1, args.device_batch_size + 1):
+            passk[k-1] = sum(any(o["is_correct"] for o in r["outcomes"][:k]) for r in records)
+
+        num_records = torch.tensor(len(records) , dtype = torch.long, device=device)
+
+        if ddp:
+            dist.all_reduce(num_records, op=dist.ReduceOp.SUM)
+            dist.all_reduce(passk, op=dist.ReduceOp.SUM)
+
+        assert num_records.item() > 0
+        passk = passk/ num_records.item()
+        print_passk = [f"Pass@{k}: {passk[k-1].item():.4f}" for k in range(1, args.device_batch_size +1)]
+        print0(f"Step {step} | { ', '.join(print_passk)}")
+        log_passk = {f"pass@{k}" : passk[k-1].item() for k in range(1,args.device_batch_size +1)}
+
+        wandb_run.log({
+            "step" : step,
+            **log_passk
+        })
+
+        rewards_list =[]
+        sequence_lengths =[]
+
+        for example_step in range(examples_per_rank):
+            sequences_all , inputs_all, targets_all, rewards_all, advantages_all = next(batch_iterator)
+
+            model.train()
+
+            assert inputs_all.size(0) % args.device_batch_size == 0
+
+            num_passes = inputs_all.size(0) // args.device_batch_size
+            for pass_idx in range(num_passes):
+                b0,b1 = pass_idx * args.device_batch_size, (pass_idx + 1) * args.device_batch_size
+                inputs=inputs_all[b0:b1]
+                targets= targets_all[b0:b1]
+                rewards = rewards_all[b0:b1]
+                advantages = advantages_all[b0:b1]
+
+                logp = -model(inputs, targets, loss_reduction='none').view_as(inputs)
+
+                pg_obj = (logp * advantages.unsqueeze(-1)).sum()
+
+                num_valid = (targets >=0).sum().clamp(min=1)
+                pg_obj = pg_obj / (num_valid * num_passes * examples_per_rank)
+
+                loss = -pg_obj
+                loss.backward()
+                print0(f"Step {step}/{num_steps} | Example step {example_step} | Pass {pass_idx} | loss: {loss.item():.6f} | Average reward: {rewards.mean().item()}")
+
+            rewards_list.append(rewards_all.mean().item())
+            sequence_lengths.extend(len(seq) for seq in sequences_all)
+
+        mean_reward = sum(rewards_list) / len(rewards_list)
+        mean_sequence_length = sum(sequence_lengths) / len(sequence_lengths)
+
+        if ddp:
+            mean_reward_tensor = torch.tensor(mean_reward, dtype=torch.float, device=device)
+            mean_sequence_length_tensor = torch.tensor(mean_sequence_length, dtype = torch.float, device=device)
+            dist.all_reduce(mean_reward_tensor, op=dist.ReduceOp.AVG)
+            dist.all_reduce(mean_sequence_length, op=dist.ReduceOp.AVG)
+            mean_reward = mean_reward_tensor.item()
+            mean_sequence_length = mean_sequence_length_tensor.item()
+
+        print0(f"Step {step}/{num_steps} | Average reward: {mean_reward} | Average sequence length: {mean_sequence_length:.2f}")
+
+        wandb_run.log({
+            "step": step,
+            "reward" : mean_reward,
+            "sequence_length" : mean_sequence_length,
+        })
+
+        lrm = get_lr_multiplier(step)
+        for group in optimizer.param_groups:
+            group["lr"] = group["initial_lr"] * lrm
+
+        optimizer.step()
+        model.zero_grad(set_to_none=True)
+        wandb_run.log({
+            "step":step,
+            "lrm" : lrm,
+        })
+
+        if master_process and ((step>0 and step % args.save_every==0) or step==num_steps -1):
+            base_dir = get_base_dir()
+            depth = model.config.n_layer
+            output_dirname = args. model_tag if args.model_tag else f"d{depth}"
+            checkpoint_dir = os.path.join(base_dir, "chatrl_checkpoints", output_dirname)
+            model_config_kwargs = model.config.__dict__
+            save_checkpoint(
+            checkpoint_dir,
+            step,
+            model.state_dict(),
+            None, # note: we don't bother to save the optimizer state
+            {
+                "model_config": model_config_kwargs,
+            }
+        )
+        print(f"✅ Saved model checkpoint to {checkpoint_dir}")
+
+wandb_run.finish() # wandb run finish
+compute_cleanup()
+
+
+
+
 
 
 
